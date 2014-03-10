fcc6bfa9a4d3464bb581d0f56657bc31ebdce10c
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-03-10 20:03:55 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='django-FIUT',
+    version='0.1.0',
+    author=u'Rafał Mirończyk',
+    author_email='voyageur.pl@gmail.com',
+    license='MIT',
+    description='Fine, Incredibly Useful Tools',
+    packages=['fiut'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
